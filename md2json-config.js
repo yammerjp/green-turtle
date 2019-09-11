6417308079a30a7062f4e100ac619607a3d88ebc
Keisuke Nakayama <baskk4.dt@gmail.com>
2019-09-11 22:43:50 +0900

    Add md2json

---
+++ md2json-config.js
@@ -0,0 +1,6 @@
+export default {
+  articleFolderPath: 'assets/article',
+  mdFileName: 'index.md',
+  jsonFileName: 'index.json',
+  extendsNuxtConfig: 'article-ids-extends-nuxt-config.js'
+}
---
+++ md2json.js
@@ -0,0 +1,78 @@
+const fs = require('fs')
+const yamlFront = require('yaml-front-matter')
+const marked = require('marked')
+
+const config = require('./md2json-config.js')
+
+fs.readdir(config.articleFolderPath,
+  (err, files) => {
+    // eslint-disable-next-line no-console
+    if (err) {
+      // eslint-disable-next-line no-console
+      console.log(err)
+    }
+    const articleIds = files.filter(file => isExistFile(`${config.articleFolderPath}/${file}/${config.mdFileName}`))
+    md2jsons(articleIds)
+  }
+)
+
+const md2json = (inputFileName, outputFileName) => {
+  fs.readFile(inputFileName, 'utf-8', (err, fileContents) => {
+    if (err !== null) {
+      // eslint-disable-next-line no-console
+      console.log(err)
+      return
+    }
+    if (fileContents === undefined) {
+      // eslint-disable-next-line no-console
+      console.log('fileContents is undefind')
+      return
+    }
+
+    const obj = yamlFront.loadFront(fileContents)
+    if (obj.__content === undefined) {
+      // eslint-disable-next-line no-console
+      console.log('Error: __content is undefind')
+      return
+    }
+
+    obj.html = marked(obj.__content)
+    delete obj.__content
+    obj.summary = obj.html.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, ' ').slice(0, 50)
+
+    const json = JSON.stringify(obj)
+
+    fs.writeFile(outputFileName, json, (err) => {
+      if (err) {
+        // eslint-disable-next-line no-console
+        console.log(err)
+      }
+    })
+  })
+}
+
+const md2jsons = (articleIds) => {
+  let articleIdsJsContent = 'export default ['
+  articleIds.forEach((articleId, idx) => {
+    md2json(`${config.articleFolderPath}/${articleId}/${config.mdFileName}`, `${config.articleFolderPath}/${articleId}/${config.jsonFileName}`)
+
+    articleIdsJsContent += `'/article/${articleId}' ${(idx === articleIds.length - 1) ? ']' : ','}`
+  })
+  fs.writeFile(config.extendsNuxtConfig, articleIdsJsContent, (err) => {
+    if (err) {
+      // eslint-disable-next-line no-console
+      console.log(err)
+    }
+  })
+}
+
+function isExistFile (file) {
+  try {
+    fs.statSync(file)
+    return true
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      return false
+    }
+  }
+}
