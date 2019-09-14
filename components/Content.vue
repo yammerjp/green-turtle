665521217a46d04769a6379826d862cc9fcce636
Keisuke Nakayama <baskk4.dt@gmail.com>
2019-09-14 12:37:02 +0900

    Devide conponents Content

---
+++ components/Content.vue
@@ -0,0 +1,46 @@
+<template>
+    <div :class="{
+      'gt-content':true,
+      'gt-content-header-navigation':isHeaderNavigation,
+      'gt-content-non-header-navigation': !isHeaderNavigation
+      }">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        isHeaderNavigation : false
+    }
+}
+</script>
+
+<style>
+.gt-content{
+  max-width: 800px;
+  word-wrap: break-word;
+  margin: auto;
+  background-color: #ffffff;
+  border: solid;
+  border-radius: 0.2em;
+  border-width: 1px;
+  border-color: #aaaaaa;
+  margin-bottom: 1em;
+}
+.gt-content-header-navigation{
+  margin-top: 0;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+  border-top-style: none;
+  text-align: center;
+  padding: 0.1em;
+  padding-bottom: 0.2m;
+  font-size: 1.3em;
+}
+.gt-content-non-header-navigation{
+  margin-top: 1em;
+  padding: 1em;
+}
+
+</style>
