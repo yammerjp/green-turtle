7c81da6f21235a73cdbe05cb31b7f8a3efe94751
Keisuke Nakayama <baskk4.dt@gmail.com>
2019-09-14 09:00:37 +0900

    Add article css

--- components/Article.vue
+++ components/Article.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-html="articleObject.html" />
+  <div v-html="articleObject.html" class="articleBox"/>
 </template>
 
 <script>
@@ -11,3 +11,38 @@ export default {
 }
 
 </script>
+
+<style>
+.articleBox{
+  max-width: 800px;
+  word-wrap: break-word;
+  margin: auto;
+  padding: 1em;
+}
+img{
+  width: 90%;
+  display: block;
+  margin: auto;
+  margin-top: 1em;
+  margin-bottom: 1em;
+}
+h2{
+  margin-top: 1em;
+  border-bottom: solid;
+  border-left-style: solid;
+  border-left-width: 0.5em;
+  border-color: #b3b3b3;
+  padding-left: 0.5em;
+}
+h3{
+  margin-top: 0.5em;
+  border-bottom: solid;
+  border-left-style: solid;
+  border-left-width: 0.5em;
+  border-color: #b3b3b3;
+  padding-left: 0.5em;
+}
+h4{
+  margin-top: 0.2em;
+}
+</style>
