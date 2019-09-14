bf5971be4e33c3b29041dd873b58858cdf5e2709
Keisuke Nakayama <baskk4.dt@gmail.com>
2019-09-15 01:27:47 +0900

    Add Top page of the same as newest article

--- copyPhoto.js
+++ copyPhoto.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const config = require('./md2json-config.js')
 const articleIdPaths = require(config.extendsNuxtConfig)
+const allArticles = require('./assets/allArticles.json')
 
 const copyArticlePhotos = (articleIdPath) => {
   fs.readdir(`assets${articleIdPath}`, (err, files) => {
@@ -13,14 +14,21 @@ const copyArticlePhotos = (articleIdPath) => {
       const copyFrom = `assets${articleIdPath}/${file}`
       // ${config.generateDir}と${articleIdPath}の間に/がないのは正常 ${articleIdPath}が/を内包
       const copyTo = `${config.generateDir}${articleIdPath}/${file}`
-      fs.copyFile(copyFrom, copyTo, (err) => {
-        if (err) {
-          console.log(err)
-          throw err
-        }
-        console.log(`Copy: ${copyFrom} => ${copyTo}`)
-      })
+      copy(copyFrom, copyTo)
+      if (articleIdPath === `/article/${allArticles[0].id}`) {
+        copy(copyFrom, `${config.generateDir}/${file}`)
+      }
     })
+  })
+}
+
+const copy = (copyFrom, copyTo) => {
+  fs.copyFile(copyFrom, copyTo, (err) => {
+    if (err) {
+      console.log(err)
+      throw err
+    }
+    console.log(`Copy: ${copyFrom} => ${copyTo}`)
   })
 }
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,60 @@
 <template>
   <div>
-    top page
+    <Header />
+    <Article :article-id="$route.params.id" :article-object="articleObject" :prev-and-next-article-id="prevAndNextArticleId"/>
     <FirebaseScript />
   </div>
 </template>
 
 <script>
+import Header from '~/components/Header.vue'
+import Article from '~/components/Article.vue'
 import FirebaseScript from '~/components/FirebaseScript.vue'
 export default {
   components: {
+    Header,
+    Article,
     FirebaseScript
+  },
+  asyncData () {
+    // generateするのでファイルが存在しない場合のエラー処理は考慮しない
+    const allArticles = require(`~/assets/allArticles.json`)
+    const articleObject = require(`~/assets/article/${allArticles[0].id}/index.json`)
+    return { articleObject, allArticles }
+  },
+  computed: {
+    prevAndNextArticleId() {
+      let number = undefined
+      for (let i = 0; i < this.allArticles.length; i++) {
+        if (this.allArticles[i].id === this.articleObject.id) {
+          number = i
+          break
+        }
+      }
+      if (number === undefined) {
+        const e = "Error: article is not found"
+        console.log(e)
+//        throw e
+        return undefined
+      }
+      return {
+        next : (number === 0) ? undefined : this.allArticles[number-1].id,
+        prev : (number === this.allArticles.length-1) ? undefined : this.allArticles[number+1].id
+      }
+    }
   }
 }
 </script>
 
-<style scoped>
+<style>
+a {
+  color: #808080;
+}
+a:visited {
+  color: #aaaaaa
+}
+body{
+  background-color: #f8f8f8;
+}
 </style>
+
